0166036ee8decffd2e290370ecea3cbc9c0efaff
Alona Daphna <alona.daphna@gmail.com>
2024-05-03 22:52:29 +0300

    remove media query from index.css

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     allowedHeaders: '*',
   })
